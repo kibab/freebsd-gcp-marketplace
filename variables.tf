aa337da913fa9aecd6d6bc3abe39fe9d5a6347ed
Ilya Bakulin <ilya@bakulin.de>
2025-03-31 22:13:57 +0200

    Import unmodified autogenerated TF module

---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
+
+// Marketplace requires this variable name to be declared
+variable "goog_cm_deployment_name" {
+  description = "The name of the deployment and VM instance."
+  type        = string
+}
+
+variable "source_image" {
+  description = "The image name for the disk for the VM instance."
+  type        = string
+  default     = "projects/freebsd-org-cloud-dev/global/images/freebsd-13-5-release-amd64-gce"
+}
+
+variable "zone" {
+  description = "The zone for the solution to be deployed."
+  type        = string
+  default     = "us-west1-c"
+}
+
+variable "machine_type" {
+  description = "The machine type to create, e.g. e2-small"
+  type        = string
+  default     = "n2d-standard-2"
+}
+
+variable "boot_disk_type" {
+  description = "The boot disk type for the VM instance."
+  type        = string
+  default     = "pd-standard"
+}
+
+variable "boot_disk_size" {
+  description = "The boot disk size for the VM instance in GBs"
+  type        = number
+  default     = 20
+}
+
+variable "networks" {
+  description = "The network name to attach the VM instance."
+  type        = list(string)
+  default     = ["default"]
+}
+
+variable "sub_networks" {
+  description = "The sub network name to attach the VM instance."
+  type        = list(string)
+  default     = []
+}
+
+variable "external_ips" {
+  description = "The external IPs assigned to the VM for public access."
+  type        = list(string)
+  default     = ["EPHEMERAL"]
+}
+
+variable "enable_cloud_api" {
+  description = "Allow full access to all of Google Cloud Platform APIs on the VM"
+  type        = bool
+  default     = true
+}
